67027dccaed98a3c5fe027d9ccb832b6cd1083e3
Kimbug <kimbug@planetariumhq.com>
2023-08-02 18:21:58 +0900

    feat(favicon): implement favicon icon

---
+++ components/Favicon.tsx
@@ -0,0 +1,30 @@
+export default function Favicon() {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png?=v1"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png?=v1"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png?=v1"
+      />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg?=v1"
+        color="#01d1c3"
+      />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  )
+}
